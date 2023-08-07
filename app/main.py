2e9f67aa9be74155a90d1d70eb97594f7ef787df
Okite Chimaobi <okitesamuel28@gmail.com>
2023-08-07 03:16:07 +0100

    no alembic

--- app/main.py
+++ app/main.py
@@ -13,7 +13,7 @@ from .routers import course, user, auth, student, instructor, assessment, questi
 # from .config import settings
 
 
-# models.Base.metadata.create_all(bind=engine)
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
