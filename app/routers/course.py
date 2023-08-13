b451adefb3b63d33ffd02452311ca6fd3b522a47
Okite Chimaobi <okitesamuel28@gmail.com>
2023-08-13 12:19:08 +0100

    get course assessment

--- app/routers/course.py
+++ app/routers/course.py
@@ -17,19 +17,22 @@ router = APIRouter(
     tags=['Courses']
 )
 
+
 @router.post("/", response_model=schemas.CourseOut, status_code=201)
-def create_course(course:schemas.Course, db:Session=Depends(get_db),
-                   user: schemas.TokenUser = Depends(oauth2.get_current_user)):
+def create_course(course: schemas.Course, db: Session = Depends(get_db),
+                  user: schemas.TokenUser = Depends(oauth2.get_current_user)):
     if not user.is_instructor:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="not unauthorized to perform request")
-    exists = db.query(models.Course).filter(models.Course.course_code == course.course_code).count() > 0
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                            detail="not unauthorized to perform request")
+    exists = db.query(models.Course).filter(
+        models.Course.course_code == course.course_code).count() > 0
     if exists:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, 
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
                             detail=f"course with code {course.course_code} already exists")
     new_course = models.Course(**course.dict())
 
     instructor = schemas.EnrollInstructor(course_code=course.course_code,
-                                              instructor_id=user.id, is_coordinator=True, is_accepted=True)
+                                          instructor_id=user.id, is_coordinator=True, is_accepted=True)
     instructor = models.CourseInstructor(**instructor.dict())
     db.add(instructor)
     db.add(new_course)
@@ -37,49 +40,56 @@ def create_course(course:schemas.Course, db:Session=Depends(get_db),
     db.refresh(new_course)
     return new_course
 
+
 @router.put("/{code}/photo", response_model=schemas.CourseOut, status_code=201)
-async def upload_photo(code:str, file: UploadFile = File(...),
-                       user:schemas.TokenData=Depends(oauth2.get_current_user), db: Session = Depends(get_db)):
+async def upload_photo(code: str, file: UploadFile = File(...),
+                       user: schemas.TokenData = Depends(oauth2.get_current_user), db: Session = Depends(get_db)):
     instructor = db.query(models.CourseInstructor).filter(
         models.CourseInstructor.course_code == code,
         models.CourseInstructor.is_accepted == True,
         models.CourseInstructor.instructor_id == user.id).first()
-    
+
     if not instructor:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Access denied")
-    course_query = db.query(models.Course).filter(models.Course.course_code ==code)   
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Access denied")
+    course_query = db.query(models.Course).filter(
+        models.Course.course_code == code)
     response = cloudinary.uploader.upload(file.file)
     image_url = response.get("secure_url")
-    course_query.update({"course_photo_url":image_url}, synchronize_session=False)
+    course_query.update({"course_photo_url": image_url},
+                        synchronize_session=False)
     db.commit()
     return course_query.first()
 
 
 @router.put("/{code}", response_model=schemas.CourseOut, status_code=201)
-def update_course(code:str, new_course:schemas.Course,
-                       user:schemas.TokenData=Depends(oauth2.get_current_user), db: Session = Depends(get_db)):
+def update_course(code: str, new_course: schemas.Course,
+                  user: schemas.TokenData = Depends(oauth2.get_current_user), db: Session = Depends(get_db)):
     instructor = db.query(models.CourseInstructor).filter(
         models.CourseInstructor.course_code == code,
         models.CourseInstructor.is_accepted == True,
         models.CourseInstructor.instructor_id == user.id).first()
-    
+
     if not instructor:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Access denied")
-    course_query = db.query(models.Course).filter(models.Course.course_code ==code)   
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Access denied")
+    course_query = db.query(models.Course).filter(
+        models.Course.course_code == code)
     course_query.update(new_course.dict(), synchronize_session=False)
     db.commit()
     return course_query.first()
 
 
-
 @router.get("/", response_model=List[schemas.CourseOut])
-def get_courses(db:Session=Depends(get_db),
-                user: schemas.TokenUser = Depends(oauth2.get_current_user),semester: int = 1,
+def get_courses(db: Session = Depends(get_db),
+                user: schemas.TokenUser = Depends(oauth2.get_current_user), semester: int = 1,
                 title: Optional[str] = None, faculty: Optional[str] = None, level: Optional[int] = None,
-                 skip: int = 0, limit: int = 10  ):
-    courses_query = db.query(models.Course).filter(models.Course.semester == semester)
+                skip: int = 0, limit: int = 10):
+    courses_query = db.query(models.Course).filter(
+        models.Course.semester == semester)
     if title:
-        courses_query = courses_query.filter(models.Course.title.contains(title))
+        courses_query = courses_query.filter(
+            models.Course.title.contains(title))
     if faculty:
         courses_query = courses_query.filter(models.Course.faculty == faculty)
     if level:
@@ -89,22 +99,23 @@ def get_courses(db:Session=Depends(get_db),
 
 
 @router.get("/enrollments", response_model=List[schemas.CourseOut])
-def get_enrollments(db:Session=Depends(get_db),
-                user: schemas.TokenUser = Depends(oauth2.get_current_user),semester: int = 1,
-                title: Optional[str] = None, faculty: Optional[str] = None, level: Optional[int] = None,
-                skip: int = 0, limit: int = 10 ):
+def get_enrollments(db: Session = Depends(get_db),
+                    user: schemas.TokenUser = Depends(oauth2.get_current_user), semester: int = 1,
+                    title: Optional[str] = None, faculty: Optional[str] = None, level: Optional[int] = None,
+                    skip: int = 0, limit: int = 10):
     if user.is_instructor:
         courses_query = db.query(models.Course).join(models.CourseInstructor,
-                                                     models.Course.course_code == 
+                                                     models.Course.course_code ==
                                                      models.CourseInstructor.course_code).filter(
             models.CourseInstructor.instructor_id == int(user.id), models.Course.semester == semester)
     if not user.is_instructor:
         courses_query = db.query(models.Course).join(models.Enrollment,
-                                                     models.Course.course_code == 
+                                                     models.Course.course_code ==
                                                      models.Enrollment.course_code).filter(
             models.Enrollment.reg_num == int(user.id), models.Course.semester == semester)
     if title:
-        courses_query = courses_query.filter(models.Course.title.contains(title))
+        courses_query = courses_query.filter(
+            models.Course.title.contains(title))
     if faculty:
         courses_query = courses_query.filter(models.Course.faculty == faculty)
     if level:
@@ -112,8 +123,9 @@ def get_enrollments(db:Session=Depends(get_db),
     courses_query.limit(limit).offset(skip*limit)
     return courses_query.all()
 
+
 @router.get("/faculties", response_model=schemas.Faculty)
-def get_faculties(db:Session=Depends(get_db),user: schemas.TokenUser = Depends(oauth2.get_current_user)):
+def get_faculties(db: Session = Depends(get_db), user: schemas.TokenUser = Depends(oauth2.get_current_user)):
     faculties = db.query(models.Course.faculty).distinct().all()
     faculties = [f for (f, ) in faculties]
     faculties = {"faculties": faculties}
@@ -121,28 +133,49 @@ def get_faculties(db:Session=Depends(get_db),user: schemas.TokenUser = Depends(o
 
 
 @router.get("/{code}", response_model=schemas.Course)
-def get_courses(code: str, db:Session=Depends(get_db),user: schemas.TokenUser = Depends(oauth2.get_current_user)):
-    course = db.query(models.Course).filter(models.Course.course_code == code).first()
+def get_courses(code: str, db: Session = Depends(get_db), user: schemas.TokenUser = Depends(oauth2.get_current_user)):
+    course = db.query(models.Course).filter(
+        models.Course.course_code == code).first()
     if not course:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, 
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
                             detail=f"course with code {code} already exists")
     return course
 
 
+@router.get("/{code}/assessments", response_model=List[schemas.AssessmentOut])
+def get_all_assessment(code: str, db: Session = Depends(get_db),
+                       user: schemas.TokenUser = Depends(oauth2.get_current_user)):
+    if user.is_instructor:
+        instructor = db.query(models.CourseInstructor).filter(
+            models.CourseInstructor.course_code == code,
+            models.CourseInstructor.instructor_id == user.id,
+            models.CourseInstructor.is_coordinator == True).first()
+        if not instructor:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+    if not user.is_instructor:
+        student = db.query(models.Enrollment).filter(
+            models.Enrollment.reg_num == user.id, models.Enrollment.course_code == code).first()
+        if not student:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+    assessment = db.query(models.Assessment).filter(
+        models.Assessment.course_id == code).all()
+    return assessment
+
 
 @router.delete("/{code}", status_code=204)
-def delete_courses(code: str, db:Session=Depends(get_db),user: schemas.TokenUser = Depends(oauth2.get_current_user)):
+def delete_courses(code: str, db: Session = Depends(get_db), user: schemas.TokenUser = Depends(oauth2.get_current_user)):
     instructor = db.query(models.CourseInstructor).filter(
         models.CourseInstructor.course_code == code,
         models.CourseInstructor.instructor_id == user.id,
         models.CourseInstructor.is_coordinator == True).first()
-    
+
     if not instructor:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Access denied")
-    photo_path =  os.path.join(config.PHOTO_DIR, "courses", code)
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Access denied")
+    photo_path = os.path.join(config.PHOTO_DIR, "courses", code)
     if os.path.exists(photo_path):
         os.remove(photo_path)
-    course_query = db.query(models.Course).filter(models.Course.course_code ==code) 
+    course_query = db.query(models.Course).filter(
+        models.Course.course_code == code)
     course_query.delete(synchronize_session=False)
-    db.commit() 
-
+    db.commit()
