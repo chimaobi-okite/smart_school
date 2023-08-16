1b1ac37e4214c82886f99e05cd121452bddeb2ba
Okite Chimaobi <okitesamuel28@gmail.com>
2023-08-16 08:11:21 +0100

    update getting assessment question time

--- app/routers/assessment.py
+++ app/routers/assessment.py
@@ -181,12 +181,12 @@ def get_assessment_questions(id: int, db: Session = Depends(get_db),
         # if not ((current_time <= assessment_detail.end_date) and (current_time > assessment_detail.start_date)):
         #     raise HTTPException(status_code=status.HTTP_405_METHOD_NOT_ALLOWED,
         #                         detail=f"test has either ended or not started at this time:{current_time}")
-        # if assessment_detail.start_date < current_time:
-        #     raise HTTPException(status_code=status.HTTP_405_METHOD_NOT_ALLOWED,
-        #                         detail=f"test is yet to start")
-        # if current_time > assessment_detail.end_date:
-        #     raise HTTPException(status_code=status.HTTP_405_METHOD_NOT_ALLOWED,
-        #                         detail=f"test is has ended")
+        if assessment_detail.start_date > current_time:
+            raise HTTPException(status_code=status.HTTP_405_METHOD_NOT_ALLOWED,
+                                detail=f"test is yet to start")
+        if current_time > assessment_detail.end_date:
+            raise HTTPException(status_code=status.HTTP_405_METHOD_NOT_ALLOWED,
+                                detail=f"test is has ended")
         submission = db.query(models.Submission).filter(models.Submission
                                                         .assessment_id == id, models.Submission.student_id == user.id).first()
         if submission:
